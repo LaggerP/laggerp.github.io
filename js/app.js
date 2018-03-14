f1e09dc48d3cd7959becaf67e9de02528e05c191
LaggerP <pablo_lagger@hotmail.com>
2018-03-14 19:36:08 -0300

    Sexto commit
    Se modificaron cosas en el index.html (agregado de archivo app.js)

--- js/app.js
+++
@@ -1,9 +0,0 @@
-var elem = document.getElementById("myVideo");
-
-if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-} else if (elem.mozRequestFullScreen) {
-    elem.mozRequestFullScreen();
-} else if (elem.webkitRequestFullscreen) {
-    elem.webkitRequestFullscreen();
-}
